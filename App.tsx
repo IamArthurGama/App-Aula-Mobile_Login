d25268be8ddacb066593cf1f00264204f6e9ebb4
Arthur Gama <114451612+IamArthurGama@users.noreply.github.com>
2026-03-18 21:12:14 -0300

    1

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import { NavigationRoutes } from "./src/routes";
+import { Login } from "./src/screens/login";
+import "./src/styles/global.css"
+
+ 
+export default function App() {
+  return (
+    <NavigationRoutes/>
+  );
+}
